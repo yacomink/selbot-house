e1fbb5f0131eface8a4726d968af3256429d55eb
Andy Yaco-Mink <andy@buzzfeed.com>
2014-09-08 19:03:02 -0400

    add script to check for running sels

---
+++ checker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RUNNING=$(ps a | grep selenium2 | fgrep -v grep | head -1 | awk '{ print $NF }')
+BRANCH=$(cd /buzzfeed/webapp ; git branch  | fgrep '*' | awk '{ print $NF }')
+HOSTNAME=$(hostname)
+
+if [ $RUNNING ]
+then
+	echo http://selbot-house.herokuapp.com/$HOSTNAME?running=$RUNNING\&branch=$BRANCH
+fi
